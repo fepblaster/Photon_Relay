510dd9012ec14f64a3f4750baed8acff09165693
fepblaster <rvasquez@automatecno.cl>
2015-11-21 19:57:19 -0300

    Update relay-shield-demo.cpp

--- firmware/examples/relay-shield-demo.cpp
+++ firmware/examples/relay-shield-demo.cpp
@@ -1,7 +1,7 @@
 // This #include statement was automatically added by the Spark IDE.
 #include "relay-lib/relay-lib.h"
 
-#define RELAY1_PIN D0
+#define RELAY1_PIN D10
 #define RELAY2_PIN D1
 #define RELAY3_PIN D2
 #define RELAY4_PIN D3
@@ -30,6 +30,7 @@ int relayControl(String command)
   if (command.substring(3,5) == "ON") relay[relayNumber - 1].on();
   else if (command.substring(3,6) == "OFF") relay[relayNumber - 1].off();
   else if (command.substring(3,9) == "SWITCH")  relay[relayNumber - 1].toggle();
+  else if (command.substring(3,8) == "PULSE")  relay[relayNumber - 1].pulse();
   else return -1;
 
   return 1;
